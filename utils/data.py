de87fda6fed5ee7055d26d36a8d479e37f4a05e0
joshuaabalo <147736875+joshuaabalo@users.noreply.github.com>
2025-08-12 17:17:13 +0000

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import os
+
+KIDS_CSV = "data/kids.csv"
+
+def load_kids_from_csv():
+    if os.path.exists(KIDS_CSV):
+        return pd.read_csv(KIDS_CSV)
+    else:
+        return pd.DataFrame(columns=["Name", "Age", "Program"])
+
+def save_kids_to_csv(df):
+    df.to_csv(KIDS_CSV, index=False)
+
+def import_kids_from_excel(excel_file):
+    df = pd.read_excel(excel_file)
+    save_kids_to_csv(df)
+    return df
 
